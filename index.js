a190fd6fa793ac3b008d11734cb81787d5282d2b
Daniel Fagnan <dnfagnan@gmail.com>
2013-03-29 22:00:31 -0600

    new server. Deleted old files for now.
    Signed-off-by: Daniel Fagnan <dnfagnan@gmail.com>

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-module.exports = function(args) {
 
+/**
+ * @package Tower-Server
+ */
 
+module.exports = function(program) {
+  return Server.create(program);
+}
 
+/**
+ * Constructor
+ */
+
+function Server(args) {
+
+}
+
+Server.create = function(args) {
+  return new Server(args);
 };
