e81ee808314ecea862648be0a4e4cdb3c03d5ac2
Daniel Fagnan <dnfagnan@gmail.com>
2013-04-19 23:07:21 -0600

    changes
    Signed-off-by: Daniel Fagnan <dnfagnan@gmail.com>

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-var createApp = require('tower-app')
-  , express   = require('express')
-  , app       = express();
+var application = require('tower-app')
+  , express = require('express')
+  , sockjs = require('sockjs')
+  , instance = null
+  , Bundle = require('tower-bundle')
+  , http = require('http');
+
 /**
  * @package Tower-Server
  */
 
-module.exports = function(program) {
-  return Server.create(program);
+module.exports = function(args) {
+  if (instance) return instance;
+  return instance = new Server(args);
+}
+
+module.exports.instance = function() {
+  return instance;
 }
 
 /**
@@ -14,12 +23,81 @@ module.exports = function(program) {
  */
 
 function Server(args) {
-  this.environment = args.environment || "development";
-  this.app = createApp(app, this);
+  var self = this;
+
+  this.options = {
+    port: args.port || 3000,
+    environment: args.environment || "development"
+  }
+
+  this.io = sockjs.createServer({
+    sockjs_url: "http://cdn.sockjs.org/sockjs-0.3.min.js"
+    , log: function() {}
+  });
+
+  this.openSockets = [];
+  this.callbacks = [];
+  this.express = express();
+  this.bundle = new Bundle(this);
+
+  this.initializeSockets();
+
+  // Middleware.
+  // XXX: Move this into a separate method of it's own.
+  this.express.use('/public', express.static(process.cwd() + '/public'));
+
+  this.app = application;
+  this.app.init(this);
+
+
+  this.server = http.createServer(this.express);
+  this.io.installHandlers(this.server, {
+    prefix: '/echo'
+  });
 }
 
+Server.prototype.listen = function() {
+  this.server.listen(this.options.port, '0.0.0.0');
+  console.log("Tower is listening on " + this.options.port)
+};
 
+Server.prototype.initializeSockets = function() {
+  var self = this;
+  this.io.on('connection', function(socket) {
 
-Server.create = function(args) {
-  return new Server(args);
+    socket.send = function(data) {
+      socket.write(data);
+    };
+
+    socket.on('data', function(message) {
+      console.log(message);
+    });
+
+    socket.on('close', function() {
+      var open = [];
+      for (var i = 0, n = self.openSockets.length; i < n; i++) {
+        if (self.openSockets[i] !== socket) {
+          open.push(self.openSockets[i]);
+        }
+      }
+      self.openSockets = open;
+    });
+
+    self.openSockets.push(socket);
+
+    self.callbacks.forEach(function(cb) {
+      cb(socket);
+    })
+
+  });
+
+};
+
+Server.prototype.emit = function(data) {
+  var self = this;
+
+  this.openSockets.forEach(function(socket) {
+    socket.send(JSON.stringify(data));
+  });
+
 };
